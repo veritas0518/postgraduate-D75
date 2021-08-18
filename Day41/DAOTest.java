6a3cb5e70aea57acf9fe0c711d490b6a2e8a0551
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-08-18 17:02:24 +0800

    Preliminary understanding of Dao
    Some of today's content involves JDBC, but it is not very deep. You can make do with it first, including the following:
    
    1. Introduction and operation of flow
    
    2. Classification of flow
    
    3. Flow architecture
    
    4. An object of the file class, which represents a file or a file directory (commonly known as a folder)
    
    5. The file class is declared under the java.io package
    
    6. The file class involves the creation, deletion, renaming, modification time, file size and other methods of files or file directories
    
    7. Objects of subsequent file classes are often passed to the stream constructor as parameters to indicate the "end point" of reading or writing.

---
+++ Day41/DAOTest.java
@@ -0,0 +1,30 @@
+package Day41;
+
+import org.junit.Test;
+
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: DAOTest
+ * @Description:
+ * @Author: TianXing.Xue
+ * @Date: 2021/8/6 10:54
+ **/
+
+public class DAOTest {
+    @Test
+    public void test1() {
+        DAO<User> dao = new DAO<>();
+        dao.save("1001", new User(1001, 34, "周杰伦"));
+        dao.save("1002", new User(1002, 20, "昆凌"));
+        dao.save("1003", new User(1003, 25, "蔡依林"));
+
+        dao.update("1003", new User(1003, 30, "方文山"));
+        dao.delete("1002");
+
+        List<User> list = dao.list();
+        list.forEach(System.out::println);
+    }
+}
